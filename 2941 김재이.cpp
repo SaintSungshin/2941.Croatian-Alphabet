0807d1eceeaab2a76be52f7fa7d73a8ace179d20
JayKimm <44940633+JayKimm@users.noreply.github.com>
2018-11-16 12:55:48 +0900

    Add files via upload

---
+++ 2941 김재이.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int numOfAlphabet(string str)
+{
+	for (int i = 0;i < str.length(); i++)
+	{
+		if (str[i] == 'c')
+			if (str[i + 1] == '=' || '-')
+				i++;
+		s// 아악 스트링으로 하고싶은데 머리 터질거같아 
+	}
+}
+
+int main()
